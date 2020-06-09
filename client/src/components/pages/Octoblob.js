4986f81fa04b72c3d6adfddc7549c194f14cfeea
Cindy Wang <cindywang2001@gmail.com>
2020-06-08 18:52:20 -0700

    green sux

--- client/src/components/pages/Octoblob.js
+++ client/src/components/pages/Octoblob.js
@@ -5,6 +5,7 @@ import "./Octoblob.css";
 
 import Sitting_Avatar from "./art/Sitting_Avatar.png";
 import Mountain_Border from "./art/mountain_border.png";
+import Blob_Avatar from "./art/pink_blob_avatar.png";
 
 class Octoblob extends Component {
   constructor(props) {
@@ -21,24 +22,28 @@ class Octoblob extends Component {
     return(
     <>
     <div className="head-container">
-      <div className="intro-container u-textCenter">
-        <div className="intro-text">
-           Hi, I'm Cindy! 
-        </div>
-        <div className="intro-text-small">
-          I’m currently studying computer science at MIT, 
-          have one (1) angry cat, and love boba way more than I should.
-        </div>
-      </div>
-      <div className="sitting-avatar-container">
-        <img src={Sitting_Avatar} height="70%" >
+      <div className="blob-container">
+        <img src={Blob_Avatar} height="800px" >
         </img>
       </div>
     </div>
-    <div className="mountain-border">
+    <div className="intro-container">
+      <div className="intro-text">
+          Hi, I'm Cindy! 
+      </div>
+      <div className="intro-text-small">
+        I’m currently studying computer science at MIT, 
+        have one (1) angry cat, and love boba way more than I should.
+      </div>
+    </div>
+
+
+
+    {/* <div className="mountain-border">
         <img src={Mountain_Border}  width="100%">
         </img>
-    </div>
+    </div> */}
+    
     <div className="head-container">
       <> u r idiot2</>
   </div>
